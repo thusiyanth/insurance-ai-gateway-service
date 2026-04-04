7c6b5c3294d8aa620b2089e04476febc2df74b05
thusiyanth <thusiyanth02@gmail.com>
2026-04-05 00:35:06 +0530

    fix: restore missing @RestController and @RequestMapping annotations in ChatController

--- src/main/java/com/insurance/gateway/controller/ChatController.java
+++ src/main/java/com/insurance/gateway/controller/ChatController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+@RestController
+@RequestMapping("/api")
 @CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class ChatController {
